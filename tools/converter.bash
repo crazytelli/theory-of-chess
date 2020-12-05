a96ffa2a195afac751b9ba0bb79c34a07a54a5f9
Remigiusz Suwalski <remigiusz.suwalski@protonmail.ch>
2020-12-05 18:07:49 +0100

    Make the number of chessboards per row variable

--- tools/converter.bash
+++ tools/converter.bash
@@ -3,10 +3,14 @@ set -e
 
 repo_root="$(readlink -f "$(dirname "$(dirname "$0")")")"
 
+CHESSBOARDS_IN_A_ROW="${1:-4}"
+SCALEBOX_WIDTH="$(bc -l <<< "2.8/${CHESSBOARDS_IN_A_ROW}" | head -c 4)"
+MINIPAGE_WIDTH="$(bc -l <<< "1/${CHESSBOARDS_IN_A_ROW} - 0.004" | head -c 4)"
+
 {
 	counter=0
 	for opening in "${repo_root}"/src/*-o-*; do
-		if ! (( counter % 4 )); then
+		if ! (( counter % CHESSBOARDS_IN_A_ROW )); then
 			echo "\begin{figure}[H]"
 		fi
 		cat \
@@ -14,14 +18,16 @@ repo_root="$(readlink -f "$(dirname "$(dirname "$0")")")"
 			"${opening}" \
 			"${repo_root}/tools/converter-tail.txt"
 		counter=$((counter+1))
-		if ! (( counter % 4 )); then
+		if ! (( counter % CHESSBOARDS_IN_A_ROW )); then
 			echo "\end{figure}"
 			echo ""
 		fi
 	done
 
-	if (( counter % 4 )); then
+	if (( counter % CHESSBOARDS_IN_A_ROW )); then
 		echo "\end{figure}"
 		echo ""
 	fi
-} > "${repo_root}/src/open.tex"
+} | sed -r \
+	-e "s@SCALEBOX_WIDTH@${SCALEBOX_WIDTH}@g" \
+	-e "s@MINIPAGE_WIDTH@${MINIPAGE_WIDTH}@g" > "${repo_root}/src/open.tex"
