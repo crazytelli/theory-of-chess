c1e1a18186e5d0d7c63a951878c21f66bf43c93d
Remigiusz Suwalski <remigiusz.suwalski@protonmail.ch>
2020-12-06 00:58:32 +0100

    Prepare automation script for closed openings

--- tools/converter.bash
+++ tools/converter.bash
@@ -4,12 +4,19 @@ set -e
 repo_root="$(readlink -f "$(dirname "$(dirname "$0")")")"
 
 CHESSBOARDS_IN_A_ROW="${1:-4}"
+TARGET_FILE="$2"
+LETTER=$3 # o, s, c or n
 SCALEBOX_WIDTH="$(bc -l <<< "2.8/${CHESSBOARDS_IN_A_ROW}" | head -c 4)"
 MINIPAGE_WIDTH="$(bc -l <<< "1/${CHESSBOARDS_IN_A_ROW} - 0.025" | head -c 4)"
 
+if [[ -z "${TARGET_FILE}" ]]; then
+	"$0" "$CHESSBOARDS_IN_A_ROW" "${repo_root}/src/open.tex" "o"
+	exit 0
+fi
+
 {
 	counter=0
-	for opening in "${repo_root}"/src/*-o-*; do
+	for opening in "${repo_root}"/src/*-${LETTER}-*; do
 		#if ! (( counter % CHESSBOARDS_IN_A_ROW )); then
 		#  	echo "\begin{figure}[H]"
 		# fi
@@ -36,4 +43,4 @@ MINIPAGE_WIDTH="$(bc -l <<< "1/${CHESSBOARDS_IN_A_ROW} - 0.025" | head -c 4)"
 	fi
 } | sed -r \
 	-e "s@SCALEBOX_WIDTH@${SCALEBOX_WIDTH}@g" \
-	-e "s@MINIPAGE_WIDTH@${MINIPAGE_WIDTH}@g" > "${repo_root}/src/open.tex"
+	-e "s@MINIPAGE_WIDTH@${MINIPAGE_WIDTH}@g" > "${TARGET_FILE}"
