231ac3aa47ede89422f5f946b27ae028cb3f59fc
Remigiusz Suwalski <remigiusz.suwalski@protonmail.ch>
2020-12-30 00:14:25 +0100

    Refactor FEN => LaTeX converter

--- tools/converter.bash
+++ tools/converter.bash
@@ -1,33 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-repo_root="$(readlink -f "$(dirname "$(dirname "$0")")")"
-
-CHESSBOARDS_IN_A_ROW="${1:-4}"
-TARGET_FILE="$2"
-LETTER=$3 # o, s, c or n
-SCALEBOX_WIDTH="$(bc -l <<< "2.8/${CHESSBOARDS_IN_A_ROW}" | head -c 4)"
-MINIPAGE_WIDTH="$(bc -l <<< "1/${CHESSBOARDS_IN_A_ROW} - 0.025" | head -c 4)"
-
-{
-	counter=0
-	for opening in "${repo_root}"/src/*-${LETTER}-*; do
-		#if ! (( counter % CHESSBOARDS_IN_A_ROW )); then
-		#  	echo "\begin{figure}[H]"
-		# fi
+parse_openings() {
+	local counter=0
+	for opening in "${REPOSITORY_ROOT}"/src/*-${LETTER}-*; do
 		if ! grep -q '@' "$opening"; then
 			continue
 		fi
 		fen="$(awk -F '@' '/@/ {print $2}' "${opening}")"
 		cat \
-			"${repo_root}/tools/converter-head.txt" \
+			"${REPOSITORY_ROOT}/tools/converter-head.txt" \
 			"${opening}" \
-			"${repo_root}/tools/converter-tail.txt" | grep -v 'PGN' | sed -r "s@FEN_BOARD@${fen}@g"
+			"${REPOSITORY_ROOT}/tools/converter-tail.txt" | grep -v 'PGN' | sed -r "s@FEN_BOARD@${fen}@g"
 		counter=$((counter+1))
 		if ! (( counter % CHESSBOARDS_IN_A_ROW )); then
-			#echo ""
 			echo "\newline"
-			#echo ""
 		else
 			echo "\hspace{5mm}"
 		fi
@@ -36,6 +23,16 @@ MINIPAGE_WIDTH="$(bc -l <<< "1/${CHESSBOARDS_IN_A_ROW} - 0.025" | head -c 4)"
 	if (( counter % CHESSBOARDS_IN_A_ROW )); then
 		echo ""
 	fi
-} | sed -r \
+}
+
+REPOSITORY_ROOT="$(readlink -f "$(dirname "$(dirname "$0")")")"
+
+CHESSBOARDS_IN_A_ROW="${1:-4}"
+TARGET_FILE="$2"
+LETTER=$3 # o, s, c or n
+SCALEBOX_WIDTH="$(bc -l <<< "2.8/${CHESSBOARDS_IN_A_ROW}" | head -c 4)"
+MINIPAGE_WIDTH="$(bc -l <<< "1/${CHESSBOARDS_IN_A_ROW} - 0.025" | head -c 4)"
+
+parse_openings | sed -r \
 	-e "s@SCALEBOX_WIDTH@${SCALEBOX_WIDTH}@g" \
 	-e "s@MINIPAGE_WIDTH@${MINIPAGE_WIDTH}@g" > "${TARGET_FILE}"
