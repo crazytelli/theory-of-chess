7b036ee21360ce7d03c14ade174457b35c3ac059
Remigiusz Suwalski <remigiusz.suwalski@protonmail.ch>
2020-12-29 23:58:20 +0100

    Add stubs for Indian (Nimzo-) openings

--- tools/converter.bash
+++ tools/converter.bash
@@ -13,6 +13,7 @@ if [[ -z "${TARGET_FILE}" ]]; then
 	"$0" "$CHESSBOARDS_IN_A_ROW" "${repo_root}/src/open.tex" "o"
 	"$0" "$CHESSBOARDS_IN_A_ROW" "${repo_root}/src/semi.tex" "s"
 	"$0" "$CHESSBOARDS_IN_A_ROW" "${repo_root}/src/closed.tex" "c"
+	"$0" "$CHESSBOARDS_IN_A_ROW" "${repo_root}/src/indian.tex" "n"
 	exit 0
 fi
 
