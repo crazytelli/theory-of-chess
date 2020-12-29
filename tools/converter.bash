c4cd15bc961f715ddc3b29c0fa4aad9f66d35476
Remigiusz Suwalski <remigiusz.suwalski@protonmail.ch>
2020-12-30 00:14:39 +0100

    Use spaces, not tabs

--- tools/converter.bash
+++ tools/converter.bash
@@ -2,27 +2,27 @@
 set -e
 
 parse_openings() {
-	local counter=0
-	for opening in "${REPOSITORY_ROOT}"/src/*-${LETTER}-*; do
-		if ! grep -q '@' "$opening"; then
-			continue
-		fi
-		fen="$(awk -F '@' '/@/ {print $2}' "${opening}")"
-		cat \
-			"${REPOSITORY_ROOT}/tools/converter-head.txt" \
-			"${opening}" \
-			"${REPOSITORY_ROOT}/tools/converter-tail.txt" | grep -v 'PGN' | sed -r "s@FEN_BOARD@${fen}@g"
-		counter=$((counter+1))
-		if ! (( counter % CHESSBOARDS_IN_A_ROW )); then
-			echo "\newline"
-		else
-			echo "\hspace{5mm}"
-		fi
-	done
+    local counter=0
+    for opening in "${REPOSITORY_ROOT}"/src/*-${LETTER}-*; do
+        if ! grep -q '@' "$opening"; then
+            continue
+        fi
+        fen="$(awk -F '@' '/@/ {print $2}' "${opening}")"
+        cat \
+            "${REPOSITORY_ROOT}/tools/converter-head.txt" \
+            "${opening}" \
+            "${REPOSITORY_ROOT}/tools/converter-tail.txt" | grep -v 'PGN' | sed -r "s@FEN_BOARD@${fen}@g"
+        counter=$((counter+1))
+        if ! (( counter % CHESSBOARDS_IN_A_ROW )); then
+            echo "\newline"
+        else
+            echo "\hspace{5mm}"
+        fi
+    done
 
-	if (( counter % CHESSBOARDS_IN_A_ROW )); then
-		echo ""
-	fi
+    if (( counter % CHESSBOARDS_IN_A_ROW )); then
+        echo ""
+    fi
 }
 
 REPOSITORY_ROOT="$(readlink -f "$(dirname "$(dirname "$0")")")"
@@ -34,5 +34,5 @@ SCALEBOX_WIDTH="$(bc -l <<< "2.8/${CHESSBOARDS_IN_A_ROW}" | head -c 4)"
 MINIPAGE_WIDTH="$(bc -l <<< "1/${CHESSBOARDS_IN_A_ROW} - 0.025" | head -c 4)"
 
 parse_openings | sed -r \
-	-e "s@SCALEBOX_WIDTH@${SCALEBOX_WIDTH}@g" \
-	-e "s@MINIPAGE_WIDTH@${MINIPAGE_WIDTH}@g" > "${TARGET_FILE}"
+    -e "s@SCALEBOX_WIDTH@${SCALEBOX_WIDTH}@g" \
+    -e "s@MINIPAGE_WIDTH@${MINIPAGE_WIDTH}@g" > "${TARGET_FILE}"
